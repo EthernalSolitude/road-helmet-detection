62343ab4d04cc34caebeed78fc2e3ba8e2167f92
EthernalSolitude <163121500+EthernalSolitude@users.noreply.github.com>
2026-02-16 19:12:21 +0300

    Update models.py

--- models.py
+++ models.py
@@ -24,4 +24,4 @@ SessionLocal = sessionmaker(bind=engine)
 
 def init_db():
     Base.metadata.create_all(bind=engine)
-    print("✓ Таблица violations готова!")
+    print(" Таблица violations готова!")
